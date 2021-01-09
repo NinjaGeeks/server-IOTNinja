2c073c37c8f91bd3bfbd217af8053ac2238dfcf9
M4HDI <mahdicsharp0098@gmail.com>
2021-01-09 03:55:40 +0330

    Add 404 Messsage
    Add 404 Messsage

--- app.js
+++ app.js
@@ -30,7 +30,8 @@ app.use('/server', serversRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  var result = {status: "false", message: "404"};
+  res.status(404).json(result);
 });
 
 // error handler
@@ -38,7 +39,6 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
   // render the error page
   res.status(err.status || 500);
   res.render('error');
